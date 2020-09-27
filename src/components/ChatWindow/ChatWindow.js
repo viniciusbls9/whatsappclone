6d612d420769b64952c02b6d7a3886273743e492
Vinicius Benedito <beneditoloura@gmail.com>
2020-09-27 10:56:34 -0300

    Função de emoji adicionada

--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EmojiPicker from 'emoji-picker-react';
 
 import SearchIcon from '@material-ui/icons/Search';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
@@ -10,60 +11,93 @@ import MicIcon from '@material-ui/icons/Mic';
 import './styles.css';
 
 function ChatWindow() {
-  return (
-    <div className="chat-window">
-      <div className="chat-window-header">
 
-        <div className="header-info">
-          <img className="header-avatar" src="https://avatars3.githubusercontent.com/u/40433674?s=460&u=549c5647c3fbc3853cc210872ff1191aff7d03b6&v=4" alt="" />
-          <div className="header-name">Vinicius Benedito</div>
+    const [emojiOpen, setEmojiOpen] = useState(false);
+
+    const handleEmojiClick = () => {
+
+    }
+
+    const handleOpenEmoji = () => {
+        setEmojiOpen(true);
+    }
+
+    const handleCloseEmoji = () => {
+        setEmojiOpen(false);
+    }
+
+
+    return (
+        <div className="chat-window">
+            <div className="chat-window-header">
+
+                <div className="header-info">
+                    <img className="header-avatar" src="https://avatars3.githubusercontent.com/u/40433674?s=460&u=549c5647c3fbc3853cc210872ff1191aff7d03b6&v=4" alt="" />
+                    <div className="header-name">Vinicius Benedito</div>
+                </div>
+
+                <div className="header-buttons">
+                    <div className="header-btn">
+                        <SearchIcon style={{ color: '#919191' }} />
+                    </div>
+
+                    <div className="header-btn">
+                        <AttachFileIcon style={{ color: '#919191' }} />
+                    </div>
+
+                    <div className="header-btn">
+                        <MoreVertIcon style={{ color: '#919191' }} />
+                    </div>
+                </div>
+
+            </div>
+            <div className="chat-window-body"></div>
+
+            <div className="chat-emoji-area" style={{ height: emojiOpen ? '200px' : '0px' }}>
+                <EmojiPicker
+                    onEmojiClick={handleEmojiClick}
+                    disableSearchBar
+                    disableSkinTonePicker
+                />
+            </div>
+
+            <div className="chat-window-footer">
+
+                <div className="chat-footer-pre">
+
+                    <div className="header-btn"
+                        onClick={handleCloseEmoji}
+                        style={{ width: emojiOpen ? '40px' : '0px'}}
+                    >
+                        <CloseIcon style={{ color: '#919191' }} />
+                    </div>
+
+                    <div className="header-btn"
+                        onClick={handleOpenEmoji}
+                    >
+                        <InsertEmoticonIcon style={{ color: emojiOpen ? '#009688' : '#919191' }} />
+                    </div>
+
+                </div>
+                <div className="chat-footer-input-area">
+                    <input
+                        className="chat-footer-input"
+                        type="text"
+                        placeholder="Digite uma mensagem"
+                    />
+                </div>
+
+                <div className="chat-footer-pos">
+
+                    <div className="header-btn">
+                        <SendIcon style={{ color: '#919191' }} />
+                    </div>
+
+                </div>
+
+            </div>
         </div>
-
-        <div className="header-buttons">
-          <div className="header-btn">
-            <SearchIcon style={{ color: '#919191' }} />
-          </div>
-
-          <div className="header-btn">
-            <AttachFileIcon style={{ color: '#919191' }} />
-          </div>
-
-          <div className="header-btn">
-            <MoreVertIcon style={{ color: '#919191' }} />
-          </div>
-        </div>
-
-
-
-      </div>
-      <div className="chat-window-body">
-
-      </div>
-      <div className="chat-window-footer">
-
-        <div className="chat-footer-pre">
-          <div className="header-btn">
-            <InsertEmoticonIcon style={{ color: '#919191' }} />
-          </div>
-        </div>
-        <div className="chat-footer-input-area">
-          <input
-            className="chat-footer-input"
-            type="text" placeholder="Digite uma mensagem"
-          />
-        </div>
-
-        <div className="chat-footer-pos">
-
-          <div className="header-btn">
-            <SendIcon style={{ color: '#919191' }} />
-          </div>
-
-        </div>
-
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ChatWindow;
