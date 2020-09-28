6873d436e06731ef7c03545397900f94c7b57596
Vinicius Benedito <beneditoloura@gmail.com>
2020-09-28 10:30:57 -0300

    Conexão com o firebase criada

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyADN7UaIPMF2m9Qalo2aB3xBcOrT_luyeQ",
+    authDomain: "whatsappclone-a9eb1.firebaseapp.com",
+    databaseURL: "https://whatsappclone-a9eb1.firebaseio.com",
+    projectId: "whatsappclone-a9eb1",
+    storageBucket: "whatsappclone-a9eb1.appspot.com",
+    messagingSenderId: "526102062168",
+    appId: "1:526102062168:web:4ed2208f456a1aa923562d"
+};
